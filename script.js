a03be3fa9760f92784edfaa104867247f51f78b7
mekdes wale <mekdesw60@gmail.com>
2026-08-03 10:00:05 +0300

    script.js created

--- script.js
+++ script.js
@@ -7,11 +7,10 @@ function updateClock() {
     const seconds = String(now.getSeconds()).padStart(2, '0');
     document.getElementById('time').textContent = `${hours}:${minutes}:${seconds}`;
     
-    // Date
+    
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     document.getElementById('date').textContent = now.toLocaleDateString('en-US', options);
 }
 
-// Update every second
 updateClock();
 setInterval(updateClock, 1000);
